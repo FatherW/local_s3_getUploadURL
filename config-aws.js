124c4732306fee6e68c1da02a0b69e9fc343f68f
FatherW <673051389@qq.com>
2020-08-29 10:53:48 +0800

    hide password

--- config-aws.js
+++ config-aws.js
@@ -1,4 +1,4 @@
-module.exports.accessKeyId = "AKIAI6FDT6Z3WRGPB7QQ";
+module.exports.accessKeyId = "XXXXXXXXXXXX"; //把账号抄过来
 module.exports.secretAccessKey = "JGs5x2l51YD6XtO5smMtj7v/f0/nAaRDGQnjQRkZ";
 module.exports.bucketName = "guoquan-upload";
 module.exports.region = "ap-northeast-1";
