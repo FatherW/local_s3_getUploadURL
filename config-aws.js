29b8825608033fd589d1fc794e349c91ea624313
FatherW <673051389@qq.com>
2020-08-29 10:46:26 +0800

    first commit

---
+++ config-aws.js
@@ -0,0 +1,4 @@
+module.exports.accessKeyId = "AKIAI6FDT6Z3WRGPB7QQ";
+module.exports.secretAccessKey = "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";
+module.exports.bucketName = "guoquan-upload";
+module.exports.region = "ap-northeast-1";
